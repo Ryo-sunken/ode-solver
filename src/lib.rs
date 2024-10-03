a01afc4dac36ed723237a966afe262b0c4208477
Ryo-sunken <bellman.toratora@gmail.com>
2024-10-04 07:41:36 +0900

    matrixのrayon実装に対応

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,34 @@ pub fn write_state<T: Float + Display, S: Write>(x: &Matrix<T>, t: T, file: &mut
     let _  = file.write_record(&line);
 }
 
+#[cfg(feature="rayon")]
+pub trait EulerSolver<T, S>
+where
+    T: Float + Display + Send + Sync,
+    S: Write,
+{
+    fn solve(&self, x0: &Matrix<T>, dt: T, t_end: T, result_file: &mut Writer<S>) {
+        let mut t = T::zero();
+        let mut x = x0.clone();
+
+        write_state(&x, t, result_file);
+
+        while t <= t_end {
+            let dx = Self::dot_x(self, &x, t) * dt;
+
+            x = Self::post_process(self, &(&x + dx));
+            t = t + dt;
+
+            write_state(&x, t, result_file);
+        }
+    }
+
+    fn dot_x(&self, x: &Matrix<T>, t: T) -> Matrix<T>;
+
+    fn post_process(&self, x: &Matrix<T>) -> Matrix<T>;
+}
+
+#[cfg(not(feature="rayon"))]
 pub trait EulerSolver<T, S>
 where
     T: Float + Display,
@@ -41,6 +69,43 @@ where
     fn post_process(&self, x: &Matrix<T>) -> Matrix<T>;
 }
 
+#[cfg(not(feature="rayon"))]
+pub trait RungeKuttaSolver<T, S>
+where 
+    T: Float + Display + Send + Sync,
+    S: Write,
+{
+    fn solve(&self, x0: &Matrix<T>, dt: T, t_end: T, result_file: &mut Writer<S>) {
+        let mut t = T::zero();
+        let mut x = x0.clone();
+        
+        write_state(&x, t, result_file);
+
+        let two = T::one() + T::one();
+        let half = T::one() / two;
+        let six = two + two + two;
+
+        while t <= t_end {
+            let k1 = Self::dot_x(self, &x, t) * dt;
+            let k2 = Self::dot_x(self, &(&x + &k1 * half), t + dt * half) * dt;
+            let k3 = Self::dot_x(self, &(&x + &k2 * half), t + dt * half) * dt;
+            let k4 = Self::dot_x(self, &(&x + &k3), t + dt) * dt;
+            
+            let dx = (k1 + k2 * two + k3 * two + k4) / six;
+
+            x = Self::post_process(self, &(&x + dx));
+            t = t + dt;
+            
+            write_state(&x, t, result_file);
+        }
+    }
+
+    fn dot_x(&self, x: &Matrix<T>, t: T) -> Matrix<T>;
+
+    fn post_process(&self, x: &Matrix<T>) -> Matrix<T>;
+}
+
+#[cfg(not(feature="rayon"))]
 pub trait RungeKuttaSolver<T, S>
 where 
     T: Float + Display,
